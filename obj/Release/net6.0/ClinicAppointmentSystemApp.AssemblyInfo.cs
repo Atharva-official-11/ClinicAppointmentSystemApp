1127a6077c1b65be363c5621cefc2c1b0cacb214
Atharva-official-11 <levidemo1234@gmail.com>
2025-03-26 00:15:07 +0530

    update

--- obj/Release/net6.0/ClinicAppointmentSystemApp.AssemblyInfo.cs
+++ obj/Release/net6.0/ClinicAppointmentSystemApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicAppointmentSystemApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8e7521a9277dba69c23cbc95e191a13c1f644e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicAppointmentSystemApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicAppointmentSystemApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
